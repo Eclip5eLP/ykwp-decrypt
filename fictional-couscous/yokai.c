e2cfbfb9e87678c497ac75d3288a102df9105712
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2023-07-15 01:16:19 +0200

    Initial commit

---
+++ fictional-couscous/yokai.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "aes.h"
+
+typedef struct {
+    const char    *ext_in;
+    const char    *ext_out;
+    const uint8_t *key;
+} crypt_params_t;
+
+/* Constant keys for sound and encrypted zips */
+const uint8_t sound_key[] = { 0x80, 0xF0, 0x08, 0x39, 0x4E, 0xB0, 0x2F, 0x4F, 0xC7, 0xF5, 0xA5, 0xC2, 0x35, 0xC4, 0x29, 0x18 };
+const uint8_t ez_key[]    = { 0x2A, 0xB5, 0x11, 0xF4, 0x77, 0x97, 0x7D, 0x25, 0xCF, 0x6F, 0x7A, 0x8A, 0xE0, 0x49, 0xA1, 0x25 };
+const uint8_t cud_key[]   = { 0xA3, 0x3C, 0x77, 0x8C, 0x0A, 0x62, 0x5B, 0xEC, 0x69, 0x43, 0x10, 0xC2, 0xFE, 0x21, 0x56, 0x29 };
+const uint8_t nhn_key[]   = { 0xA3, 0x3C, 0x77, 0x8C, 0x0A, 0x62, 0x5B, 0xEC, 0x69, 0x43, 0x10, 0xC2, 0xFE, 0x21, 0x56, 0x29 };
+const uint8_t ayd_key[]   = { 0xA3, 0x3C, 0x77, 0x8C, 0x0A, 0x62, 0x5B, 0xEC, 0x69, 0x43, 0x10, 0xC2, 0xFE, 0x21, 0x56, 0x29 };
+
+/* Input extension, output extension, key */
+const crypt_params_t params[] = {
+    { "og", "ogg", sound_key },
+    { "m4", "m4a", sound_key },
+    { "ez", "zip", ez_key    },
+    { "cud", "json", cud_key },
+    { "nhn", "json", nhn_key  },
+    { "ayd", "txt", ayd_key  }
+};
+
+/* Isolate file extension */
+char *get_file_ext(char *filename) {
+    char *ptr;
+    size_t len;
+    
+    ptr = NULL;
+    len = strlen(filename);
+    
+    while (--len)
+    {
+        if (filename[len] == '.')
+            ptr = &filename[len + 1];
+    }
+    
+    return ptr;
+}
+
+/* Find the true last AES block length (for unpadding PKCS#7) */
+int last_block_len(uint8_t *block) {
+    int i, len;
+    
+    len = block[15];
+    
+    if (len == 0 || len > 15)
+        return 16; /* Not padded */
+    
+    for (i = 15 - 1; i > 15 - len; i--) {
+        if (block[i] != len)
+            return 16; /* Not padded */
+    }
+    
+    return len;
+}
+
+int main(int argc, char **argv) {
+    struct AES_ctx aes;
+    const crypt_params_t *param;
+    char *ext;
+    char fn[FILENAME_MAX];
+    FILE *in, *out;
+    uint8_t iv[AES_KEYLEN];
+    uint8_t *buf;
+    int i, len;
+    
+    if (argc != 2) {
+        printf("Usage: %s input\n", argv[0]);
+        return 0;
+    }
+    
+    if ((in = fopen(argv[1], "rb")) == NULL) {
+        perror("Error opening input");
+        return 1;
+    }
+    
+    if ((ext = get_file_ext(argv[1])) == NULL) {
+        fprintf(stderr, "File %s doesn't have an extension!\n", argv[1]);
+        return 1;
+    }
+    
+    /* Find file extension match */
+    for (param = NULL, i = 0; i < 6; i++) {
+        if (strcmp(ext, params[i].ext_in) == 0) {
+            param = &params[i]; 
+            break;
+        }
+    }
+    
+    if (param == NULL) {
+        fprintf(stderr, "Unknown file extension %s!\n", ext);
+        return 1;
+    }
+    
+    /* Build output filename */
+    memset(fn, 0, FILENAME_MAX);
+    strncpy(fn, argv[1], ext - argv[1]);
+    strcat(fn, param->ext_out);
+    
+    if ((out = fopen(fn, "wb")) == NULL) {
+        perror("Error opening output");
+        return 1;
+    }
+    
+    /* Read in IV and initialise AES context */
+    fread(iv, 1, AES_KEYLEN, in);
+    AES_init_ctx_iv(&aes, param->key, iv);
+    
+    /* Calculate data size and allocate buffer */
+    fseek(in, 0, SEEK_END);
+    len = ftell(in) - AES_KEYLEN;
+    buf = malloc(len);
+    
+    /* Seek to data start and read to buffer */
+    fseek(in, AES_KEYLEN, SEEK_SET);
+    fread(buf, 1, len, in);
+    fclose(in);
+    
+    /* Decrypt data */
+    AES_CBC_decrypt_buffer(&aes, buf, len);
+    
+    /* Remove padding from last block */
+    len -= last_block_len(buf + len - AES_BLOCKLEN);
+    
+    /* Write decrypted data */
+    fwrite(buf, 1, len, out);
+    fclose(out);
+    
+    free(buf);
+    
+    printf("Successfully decrypted %s to %s!\n", argv[1], fn);
+    
+    return 0;
+}
---
+++ lib/aes.py
@@ -0,0 +1,112 @@
+import binascii
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+def create_hex_table(file_path):
+    hex_table = []
+
+    with open(file_path, 'r',errors='replace') as file:
+        bytes_data = file.read()
+
+    num_bytes = len(bytes_data)
+    print(f"Generating possible keys from file: {file_path}")
+
+    for i in range(num_bytes - 31):
+        hex_sequence = bytes_data[i:i+32]
+        try:
+            test = binascii.unhexlify(hex_sequence).hex()
+            hex_table.append(hex_sequence)
+        except:
+            pass
+        if i % 100000 == 0:
+            progress = (i / num_bytes) * 100
+            print(f"{progress:.2f}%", end='\r')
+    print("Deleting duplicate keys")
+    hex_table=list(set(hex_table))
+    return hex_table
+
+def decrypt_file(input_file, output_file, key_hex):
+    try:
+        block_size = algorithms.AES.block_size // 8
+        key = binascii.unhexlify(key_hex)
+
+        with open(input_file, 'rb') as file:
+            ciphertext = file.read()
+
+        iv = ciphertext[:block_size]
+        ciphertext = ciphertext[block_size:]
+
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+        padding_length = plaintext[-1]
+        plaintext = plaintext[:-padding_length]
+
+        try:
+            if len(plaintext) > 0:
+                plaintext2 = plaintext.decode("utf-8")
+                with open(output_file, 'wb') as file:
+                    file.write(plaintext)
+                return True
+            else:
+               return False
+        except Exception:
+            return False
+    except Exception:
+        return False
+
+def encrypt_file(input_file, output_file, key_hex):
+    try:
+        block_size = algorithms.AES.block_size // 8
+        key = binascii.unhexlify(key_hex)
+
+        with open(input_file, 'rb') as file:
+            plaintext = file.read()
+
+        fname, file_extension = os.path.splitext(input_file)
+        header = fname + ".cud"
+        if not os.path.isfile(header):
+            print("Header file missing")
+            return False
+
+        with open(header, 'rb') as file:
+            headertext = file.read()
+
+        iv = headertext[:block_size]
+        plaintext = plaintext + (b'\x0B' * (block_size - len(plaintext) % block_size))
+
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = iv + encryptor.update(plaintext) + encryptor.finalize()
+
+        with open(output_file, 'wb') as file:
+            file.write(ciphertext)
+    except Exception as e:
+        print(e)
+        return False
+
+def main():
+    input_file = 'test.cud'
+    key_path = "libSGF.so"
+    result = create_hex_table(key_path)
+    keys = result
+    print("Testing generated keys:")
+    print()
+    for i, key_hex in enumerate(keys, 1):
+        output_file = f"{key_hex}.json"
+        success = decrypt_file(input_file, output_file, key_hex)
+        if i % 1000 == 0:
+            progress2 = (i / len(keys)) * 100
+            print(f"{progress2:.3f}%", end='\r')
+        if success:
+            print(f"Good key found: {key_hex}")
+            break
+
+    if not success:
+        print("Key not found")
+
+if __name__ == "__main__":
+    main()
+    input()
