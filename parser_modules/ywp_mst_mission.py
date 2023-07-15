81a60ba73b0af6fd331d724e2516cc46beabf8ce
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2023-07-15 16:48:23 +0200

    Example module

--- parser_modules/ywp_mst_mission.py
+++ parser_modules/ywp_mst_mission.py
@@ -1,5 +1,32 @@
-def parser(data):
-	return data
+def parser(data, dbType):
+	translate = []
+	if dbType == "dict": # cud data is dictionary
+		masterData = json.loads(data)
+		for i, x in enumerate(masterData["data"]):
+			translate.append({"index":i,"charaName":x["charaName"],"messageText":x["messageText"]})
+	else: # cud data is string
+		masterData = data.split("|")
+		for i, x in enumerate(masterData):
+			if isTranslatable(x):
+				translate.append({"index":i,"text":x})
+	return translate
 
-def unparser(data):
-	return data
+def unparser(data, dbType):
+	try:
+		if dbType == "dict": # cud data is dictionary
+			masterData = json.loads(masterDataRaw)["data"]
+			for i, x in enumerate(data):
+				for idx, y in enumerate(x):
+					if y == "index":
+						continue
+					masterData[x["index"]][y] = x[y]
+			masterData = json.dumps({"data":masterData}, ensure_ascii=False)
+		else: # cud data is string
+			masterData = masterDataRaw.split("|")
+			for i, x in enumerate(data):
+				masterData[x["index"]] = x["text"]
+			masterData = '|'.join(masterData)
+		return masterData
+	except Exception:
+		print("Invalid data")
+		return False
