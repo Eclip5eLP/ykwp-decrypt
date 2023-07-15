6e9b85c854533f5af88c67748a9d2e6e509aeee5
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2023-07-15 03:29:20 +0200

    Updated ykwp.py to support more filetypes

--- ykwp.py
+++ ykwp.py
@@ -1,7 +1,13 @@
 import sys, os
 import binascii
+import json
 sys.path.append('./lib')
 from aes import decrypt_file, encrypt_file, create_hex_table
+
+def loadKeys():
+	with open('./lib/keys.json', 'r') as f:
+		keys = json.load(f)
+	return keys
 
 def main(file, mode):
 	ftype = -1
@@ -13,31 +19,49 @@ def main(file, mode):
 		print("File not found")
 		return False
 
-	key = binascii.unhexlify("A33C778C0A625BEC694310C2FE215629").hex()
-
 	r = -1
 	if ftype == 0: # Directory
 		for f in os.listdir(file):
-			r = crypt(file + f, mode, key)
+			r = crypt(file + f, mode)
 		print("Done")
 		return r
 	elif ftype == 1: # Single file
-		r = crypt(file, mode, key)
+		r = crypt(file, mode)
 		print("Done")
 		return r
 
-def crypt(file, mode, key):
+def crypt(file, mode):
 	fname, file_extension = os.path.splitext(file)
+	keys = loadKeys()
+
+	# Check extension
+	key = None
+	target = None
+	for ext in keys["ext"]:
+		if "." + ext[(1 if mode == "-e" else 0)] == file_extension:
+			key = keys["keys"][ext[2]]
+			target = ext[(0 if mode == "-e" else 1)]
+			break
+
+	if key == None or target == None:
+		return False
+
+	print(file)
 	if mode == "-e": # Encrypt
-		if file_extension != ".json":
-			return False
-		print(file)
-		return encrypt_file(file, fname + ".cud", key)
+		return encrypt_file(file, fname + "." + target, key)
 	elif mode == "-d": # Decrypt
-		if file_extension != ".cud":
-			return False
-		print(file)
-		return decrypt_file(file, fname + ".json", key)
+		return decrypt_file(file, fname + "." + target, key)
+
+	# if mode == "-e": # Encrypt
+	# 	if file_extension != ".json":
+	# 		return False
+	# 	print(file)
+	# 	return encrypt_file(file, fname + ".cud", key)
+	# elif mode == "-d": # Decrypt
+	# 	if file_extension != ".cud":
+	# 		return False
+	# 	print(file)
+	# 	return decrypt_file(file, fname + ".json", key)
 
 if __name__ == "__main__":
 	if (len(sys.argv) != 3) or (sys.argv[1] != "-e" and sys.argv[1] != "-d"):
